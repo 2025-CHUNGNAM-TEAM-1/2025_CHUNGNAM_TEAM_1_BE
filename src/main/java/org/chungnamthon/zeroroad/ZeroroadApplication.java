9a82f08dd529a3c69975750f4b1cae7f3b8fc9d6
kwakseobang <iii6602@naver.com>
2025-07-15 11:59:54 +0900

    git initialize

---
+++ src/main/java/org/chungnamthon/zeroroad/ZeroroadApplication.java
@@ -0,0 +1,13 @@
+package org.chungnamthon.zeroroad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroroadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroroadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/chungnamthon/zeroroad/ZeroroadApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chungnamthon.zeroroad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroroadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
